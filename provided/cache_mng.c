74b4ca589f6b4282d4c38c927dddc6c2f8381631
chaps <jean-cedric.chappelier@epfl.ch>
2019-05-06 17:17:35 +0200

    week 11+ material (reviewed + new)

---
+++ provided/cache_mng.c
@@ -0,0 +1,66 @@
+/**
+ * @file cache_mng.c
+ * @brief cache management functions
+ *
+ * @author Mirjana Stojilovic
+ * @date 2018-19
+ */
+
+#include "error.h"
+#include "util.h"
+#include "cache_mng.h"
+
+#include <inttypes.h> // for PRIx macros
+
+//=========================================================================
+#define PRINT_CACHE_LINE(OUTFILE, TYPE, WAYS, LINE_INDEX, WAY, WORDS_PER_LINE) \
+    do { \
+            fprintf(OUTFILE, "V: %1" PRIx8 ", AGE: %1" PRIx8 ", TAG: 0x%03" PRIx16 ", values: ( ", \
+                        cache_valid(TYPE, WAYS, LINE_INDEX, WAY), \
+                        cache_age(TYPE, WAYS, LINE_INDEX, WAY), \
+                        cache_tag(TYPE, WAYS, LINE_INDEX, WAY)); \
+            for(int i_ = 0; i_ < WORDS_PER_LINE; i_++) \
+                fprintf(OUTFILE, "0x%08" PRIx32 " ", \
+                        cache_line(TYPE, WAYS, LINE_INDEX, WAY)[i_]); \
+            fputs(")\n", OUTFILE); \
+    } while(0)
+
+#define DUMP_CACHE_TYPE(OUTFILE, TYPE, WAYS, LINES, WORDS_PER_LINE)  \
+    do { \
+        for(uint16_t index = 0; index < LINES; index++) { \
+            foreach_way(way, WAYS) { \
+                fprintf(output, "%02" PRIx8 "/%04" PRIx16 ": ", way, index); \
+                PRINT_CACHE_LINE(OUTFILE, const TYPE, WAYS, index, way, WORDS_PER_LINE); \
+            } \
+        } \
+    } while(0)
+
+//=========================================================================
+// see cache_mng.h
+int cache_dump(FILE* output, const void* cache, cache_t cache_type)
+{
+    M_REQUIRE_NON_NULL(output);
+    M_REQUIRE_NON_NULL(cache);
+
+    fputs("WAY/LINE: V: AGE: TAG: WORDS\n", output);
+    switch (cache_type) {
+    case L1_ICACHE:
+        DUMP_CACHE_TYPE(output, l1_icache_entry_t, L1_ICACHE_WAYS,
+                        L1_ICACHE_LINES, L1_ICACHE_WORDS_PER_LINE);
+        break;
+    case L1_DCACHE:
+        DUMP_CACHE_TYPE(output, l1_dcache_entry_t, L1_DCACHE_WAYS,
+                        L1_DCACHE_LINES, L1_DCACHE_WORDS_PER_LINE);
+        break;
+    case L2_CACHE:
+        DUMP_CACHE_TYPE(output, l2_cache_entry_t, L2_CACHE_WAYS,
+                        L2_CACHE_LINES, L2_CACHE_WORDS_PER_LINE);
+        break;
+    default:
+        debug_print("%d: unknown cache type", cache_type);
+        return ERR_BAD_PARAMETER;
+    }
+    putc('\n', output);
+
+    return ERR_NONE;
+}
